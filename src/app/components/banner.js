cada8fc9d376954a3a5c57a4f871dad5ebee94e0
Stephen <saintobis11@gmail.com>
2024-04-23 21:59:57 +0100

    discord name3

--- src/app/components/banner.js
+++ src/app/components/banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
                 <h3 className="text-xl max-[470px]:text-[14px] max-sm:text-lg leading-loose">Profitable dropshipping stores based on existing top selling shopify stores,<br/> so you are ready to start getting sales!</h3>
                 <div>
                <Link href="https://discordapp.com/users/1146887824445022268" target="_blank"><button className="bg-blue-600 h-16 w-56 rounded-md text-lg font-medium mt-5 text-white">BUILD MY STORE</button></Link> 
-               <p className="mt-5 text-[12px] font-semibold">PS: If not redirected to the agency"s discord, please add up &quot;theodore_.1&quot; on discord to communicate with us.</p>
+               <p className="mt-5 text-[12px] font-semibold">PS: If not redirected to the agency&apos;s discord, please add up &quot;theodore_.1&quot; on discord to communicate with us.</p>
                 </div>
             </div>
         </div>
